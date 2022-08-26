f8c28752b298580c434bae74ea94e5a063bb35f9
ʕ·͡˔·Ɂ <97666035+YuxZhixJiang@users.noreply.github.com>
2022-08-26 13:20:34 +0800

    Delete information directory

--- information/information.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['username']);
-    unset($_SESSION['password']);
-    echo "<script>alert('退出登录成功！');window.location.href='../login/login.html'</script>";
-?>
